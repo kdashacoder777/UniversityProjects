f666a0cd9738069a41669f9fd2aa68581c0ffd48
kdashacoder777 <166551395+kdashacoder777@users.noreply.github.com>
2024-04-12 01:45:18 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "task1.h"
+#include "task2.h"
+
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Задание 1 запуск" << std::endl;
+    task1();
+    std::cout << "Задание 1 успешно закончено" << std::endl;
+
+    std::cout << "Задание 2 запуск" << std::endl;
+    task2();
+    std::cout << "Задание 2 успешно закончено" << std::endl;
+
+
+    return 0;
+}
