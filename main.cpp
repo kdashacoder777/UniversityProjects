31ab871f88cd4500160569094ab950df7fa89893
kdashacoder777 <166551395+kdashacoder777@users.noreply.github.com>
2024-04-17 20:41:08 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ int main() {
 
     setlocale(LC_ALL, "Russian");
     
-    std::cout << "Task 1 run" << std::endl;
-    task1();
-    std::cout << "Task 1 completed successfully" << std::endl;
+    // std::cout << "Task 1 run" << std::endl;
+    // task1();
+    // std::cout << "Task 1 completed successfully" << std::endl;
 
-    std::cout << "Task 2 start" << std::endl;
-    task2();
-    std::cout << "Task 2 completed successfully" << std::endl;
+    // std::cout << "Task 2 start" << std::endl;
+    // task2();
+    // std::cout << "Task 2 completed successfully" << std::endl;
 
     std::cout << "Task 3 launch" << std::endl;
     task3();
